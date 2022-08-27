0de3a344f0bc4443f7f4c034f84ba08df34f84c3
AmanIITP <95214630+AmanIITP@users.noreply.github.com>
2022-08-28 00:06:44 +0530

    Update Sorting_Methods.c
    Added Another Sorting Method - Merge Sort

--- Sorting_Methods.c
+++ Sorting_Methods.c
@@ -3,12 +3,77 @@
 #include <stdio.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// MERGE SORT
+//  Merge two subarrays L and M into arr
+void merge(int arr[], int p, int q, int r)
+{
+
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[n1], M[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
+
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = p;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= M[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // BUBBLE SORT
 //  perform the bubble sort
 void bubbleSort(int array[], int size)
 {
 
-        for (int step = 0; step < size - 1; ++step)
+    for (int step = 0; step < size - 1; ++step)
     {
         for (int i = 0; i < size - step - 1; ++i)
         {
@@ -89,7 +154,7 @@ int main()
     int arr[n];
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-    bubbleSort(arr, n);
+    mergeSort(arr, 0, n - 1);
     printArray(arr, n);
 
     return 0;
