9e10f05a4df7659e42472dd572a2ba2f6e006d4d
AmanIITP <95214630+AmanIITP@users.noreply.github.com>
2022-08-27 23:31:07 +0530

    Add files via upload
    Added Calculator program with first operation - Addition

---
+++ Calculator.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void calculator(int num1, int num2, int opt)
+{
+    switch (opt)
+    {
+    case 1:
+        printf("The Addition of  %d and %d is: %d\n", num1, num2, num1 + num2);
+        break;
+
+    case 2:
+        printf("Thank you for using!");
+        break;
+
+    default:
+        printf("Invalid option!!\n");
+        break;
+    }
+}
+
+int main()
+{
+    int num1, num2, opt;
+    printf("Enter the first Integer :");
+    scanf("%d", &num1);
+    printf("Enter the second Integer :");
+    scanf("%d", &num2);
+
+    printf("\nInput your option :\n");
+    printf("1-Addition.\n2-Exit.\n");
+    scanf("%d", &opt);
+    calculator(num1, num2, opt);
+    return 0;
+}
